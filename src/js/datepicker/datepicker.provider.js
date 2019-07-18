d8975ad5dc929025d5a835923f6873b31536e3f8
Mateusz Rohde <mateusz.rohde@gmail.com>
2019-07-18 09:54:01 +0200

    datePickerProvider - updateDateTranslations function

--- src/js/datepicker/datepicker.provider.js
+++ src/js/datepicker/datepicker.provider.js
@@ -12,23 +12,30 @@
 			maxDate: undefined,
 			showIcon: true,
 			hideOnPick: false,
-			dayNames: DateExtended.getDayShortNames(),
+			dayNames: [],
 			monthNames: [],
-			format: 'Y-m-d'
-		};
-		this.options.dayNames.push(this.options.dayNames.shift());
-		for(let i = 0; i < 3; i++){
-			let row = [];
-			for(let j = 0; j < 4; j++){
-				let number = (i * 4) + j + 1;
-				row.push({
-					name: DateExtended.getMonthShortNames()[number - 1],
-					number: number
-				});
+			format: 'Y-m-d',
+			/**
+			 * Call this whenever you change default locale in DateExtended
+			 */
+			updateDateTranslations: () => {
+				this.options.dayNames = DateExtended.getDayShortNames();
+				this.options.dayNames.push(this.options.dayNames.shift());
+				for(let i = 0; i < 3; i++){
+					let row = [];
+					for(let j = 0; j < 4; j++){
+						let number = (i * 4) + j + 1;
+						row.push({
+							name: DateExtended.getMonthShortNames()[number - 1],
+							number: number
+						});
+					}
+					this.options.monthNames.push(row);
+					row = [];
+				}
 			}
-			this.options.monthNames.push(row);
-			row = [];
-		}
+		};
+		this.options.updateDateTranslations();
 		// noinspection JSUnusedGlobalSymbols
 		this.$get = function(){
 			return this.options;
